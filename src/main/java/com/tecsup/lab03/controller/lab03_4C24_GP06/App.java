6b7993f881bf0f89075cc96dade2b9f69f01742e
Alumno <Alumno@LAB1507-24>
2023-04-11 14:12:25 -0500

    First commit

---
+++ src/main/java/com/tecsup/lab03/controller/lab03_4C24_GP06/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab03.controller.lab03_4C24_GP06;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
